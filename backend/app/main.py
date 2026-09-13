5f77d624cd90f1321b944f6be54931b1ab6a9949
aqsakhan22 <aqsaofficaildev@gmail.com>
2026-09-13 17:02:05 +0500

    vercel url issue so;=lved

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         
-        "https://ai-customer-support-copilot-tan.vercel.app/",
+        "https://ai-customer-support-copilot-tan.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
