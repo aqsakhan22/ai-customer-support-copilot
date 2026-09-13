d695c1bd6fd5d4744e2d2f0967a6308e50c110a2
aqsakhan22 <aqsaofficaildev@gmail.com>
2026-09-13 17:26:48 +0500

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "http://localhost:5173",
         
         "https://ai-customer-support-copilot-tan.vercel.app",
+        "https://ai-customer-support-copilot-sandy.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
